c3ee92e6dcb8b7b0cab391f23e4906d29f03c36b
khalidhmd <khalidhmd@gmail.com>
2023-08-10 22:51:54 +0300

    return jwt when user auth

--- src/routes/api/auth.ts
+++ src/routes/api/auth.ts
@@ -1,14 +1,20 @@
 import express from "express";
-import bcrypt from "bcrypt";
 import { UserModel } from "../../models/User";
+import jwt from "jsonwebtoken";
 
 const auth = express.Router();
 
 //auth route
 auth.post("/", async (req, res) => {
-  const u = req.body;
-  const user = await UserModel.auth(u.email, u.password);
-  res.json(user);
+  try {
+    const u = req.body;
+    const user = await UserModel.auth(u.email, u.password);
+    if (user == null) throw new Error("bad data");
+    const token = jwt.sign({ user }, process.env.TOKEN_SECRET + "");
+    res.json(token);
+  } catch (error: any) {
+    res.status(500).json(error.message);
+  }
 });
 
 export default auth;
